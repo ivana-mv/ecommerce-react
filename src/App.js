2e6f9795a75dab62f7556dcc1ca4ad30802274d0
ivana-mv <69020774+ivana-mv@users.noreply.github.com>
2020-12-31 00:42:05 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import Titulo from './components/Titulo';
-import Menu from './components/Menu';
-import Menu2 from './components/Menu2';
+import {BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/Navbar';
 import './App.css';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Home from './Screens/Home/Index';
+import { AppProvider } from './Context/useAppContext';
+import Cart from './components/Cart/Cart';
+import HomeContainer from './components/HomeContainer/HomeContainer';
+
+
 function App() {
   return (
-    <>
-      <Titulo >
-        <Menu vestimenta="Pantalon" talle={37}/>
-        <Menu vestimenta="Remera" talle={8}/>
-        <Menu vestimenta="Buso" talle={10}/>
-      </Titulo>
-    </>
+    <AppProvider>
+      <BrowserRouter>
+      <NavBar></NavBar>
+
+        <Switch>
+          <>
+          <Route exact path= "/:type?">
+            <HomeContainer></HomeContainer>
+          </Route>
+
+          <Route exact path= "/Cart">
+            <Cart></Cart>
+          </Route>
+
+          <Route exact path= "/Detalle/:id">
+            <ItemDetailContainer></ItemDetailContainer>
+          </Route>
+          </>
+        </Switch>
+      </BrowserRouter>
+    </AppProvider>
   );
 }
+
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
 
 ReactDOM.render(
-
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
-  
   document.getElementById('root')
 );
-
-
-//Componente funcional
-//Componente de clases = se usa menos
-//Si o si tiene que haber un padre para poner hijos, sino rompe porque no se pueden tener dos padres en un solo componente
-//props: Pasan de componentes padres a componentes hijos -- es para pasar
 
